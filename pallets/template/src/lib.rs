d06291be10dc5f093b937462d320a95b0ffa3a38
沢谷 邦夫 <devel@fathens.org>
2021-12-16 19:34:54 +0900

    Add skeleton of pallet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,1 +1,24 @@
 #![cfg_attr(not(feature = "std"), no_std)]
+
+// Re-export pallet items so that they can be accessed from the crate namespace.
+pub use pallet::*;
+
+#[frame_support::pallet]
+pub mod pallet {
+    use frame_support::pallet_prelude::*;
+    use frame_system::pallet_prelude::*;
+    use sp_std::vec::Vec; // Step 3.1 will include this in `Cargo.toml`
+
+    #[pallet::config]  // <-- Step 2. code block will replace this.
+    #[pallet::event]   // <-- Step 3. code block will replace this.
+    #[pallet::error]   // <-- Step 4. code block will replace this.
+    #[pallet::pallet]
+    #[pallet::generate_store(pub(super) trait Store)]
+    #[pallet::generate_storage_info]
+    pub struct Pallet<T>(_);
+
+    #[pallet::storage] // <-- Step 5. code block will replace this.
+    #[pallet::hooks]
+    impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
+    #[pallet::call]   // <-- Step 6. code block will replace this.
+}
