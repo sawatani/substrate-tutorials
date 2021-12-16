fcfbc826eb2955cbcfa2bdad301927601ea99423
沢谷 邦夫 <devel@fathens.org>
2021-12-16 20:08:42 +0900

    Add storage of `Proofs`

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -43,7 +43,9 @@ pub mod pallet {
     #[pallet::generate_storage_info]
     pub struct Pallet<T>(_);
 
-    #[pallet::storage] // <-- Step 5. code block will replace this.
+	#[pallet::storage]
+	pub(super) type Proofs<T: Config> = StorageMap<_, Blake2_128Concat, Vec<u8>, (T::AccountId, T::BlockNumber), ValueQuery>;
+
     #[pallet::hooks]
     impl<T: Config> Hooks<BlockNumberFor<T>> for Pallet<T> {}
     #[pallet::call]   // <-- Step 6. code block will replace this.
